f30165a9d14850789b6c298c22306c380322e056
satveerkaursadhra08-ui <satveerkaursadhra08@gmail.com>
2026-08-22 22:21:25 +0530

    Add basic calculator functions for add, subtract, multiply, and divide

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function add(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerText = "Result: " + (a + b);
+}
+
+function subtract(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerText = "Result: " + (a - b);
+}
+
+function multiply(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerText = "Result: " + (a * b);
+}
+
+function divide(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerText = "Result: " + (a / b);
+}
+function clearCalculator(){
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").innerText = "Result: ";
+}
